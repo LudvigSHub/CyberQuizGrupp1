0d3511139994eb2de44a1c886f7736fe2b9c73a8
Filippa24 <filippa98b@hotmail.se>
2026-03-03 14:48:50 +0100

    databas i program.cs + Project reference till dal + loginmanager + signinmanager. fixat så login och register funkar med användare, sparas i databasen

--- CyberQuizGrupp1/Program.cs
+++ CyberQuizGrupp1/Program.cs
@@ -1,4 +1,6 @@
 
+using CyberQuizGrupp1.DAL.Data;
+using CyberQuizGrupp1.DAL.Identity;
 using CyberQuizGrupp1.UI.Components;
 using CyberQuizGrupp1.UI.Services;
 using Microsoft.AspNetCore.Identity;
@@ -10,11 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//LAGT TILL DETTA FÖR ATT ANVÄNDA IDENTITY, INSTALLERADE ENTITYFRAMEWORK OCH SQL SERVER
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 //added:
-builder.Services.AddHttpClient<AuthService>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7231/");
-});
+builder.Services.AddScoped<AuthService>();
 //added:
 builder.Services.AddHttpClient<CategoryService>(client =>
 {
@@ -25,6 +28,20 @@ builder.Services.AddHttpClient<SubCategoryService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7231/");
 });
+//added:
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    //ÄNDRA KRAV FÖR LÖSENORD:
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6; //blir minst 7 tecken (börjar på 0)
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+//added för att hantera cookies:
+builder.Services.AddHttpContextAccessor();
+//added:
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -37,11 +54,14 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
+//ADDED:
+app.UseAuthentication();
+//ADDED:
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+app.MapControllers();
 app.Run();
