9e759b12c0b79530c3776f0d8fb0fa67e0569477
Abbaskhudayer <abbas.khudayer@hotmail.com>
2026-03-05 16:22:46 +0100

    -Lagt till nya modeller

---
+++ CyberQuizGrupp1.SHARED/Models/QuizAttemptModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.SHARED.Models
+{
+    public class QuizAttemptModel 
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public int SubCategoryId { get; set; }
+        public DateTime? StartedAt { get; set; }
+        public DateTime? FinishedAt { get; set; }
+        public List<UserAnswerModel> UserAnswers { get; set; } = new ();
+     }
+}
---
+++ CyberQuizGrupp1.SHARED/Models/UserAnswerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.SHARED.Models
+{
+    public class UserAnswerModel
+    {
+        public int Id { get; set; }
+        public Guid AttemptId { get; set; }
+        public QuizAttemptModel Attempt { get; set; }
+        public string UserId { get; set; }
+        public int QuestionId { get; set; }
+        public int SelectedAnswerOptionId { get; set; }
+        public bool IsCorrect { get; set; }
+        public DateTime AnsweredAt { get; set; }
+
+    }
+}
