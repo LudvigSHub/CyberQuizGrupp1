e3214760ede66c6623345990c2b69c2a264caafa
Abbaskhudayer <abbas.khudayer@hotmail.com>
2026-03-08 15:23:50 +0100

    work in progress

---
+++ CyberQuizGrupp1.DAL/Repositories/Implementations/QuizRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.DAL.Repositories.Implementations
+{
+    internal class QuizRepository
+    {
+    }
+}
---
+++ CyberQuizGrupp1.DAL/Repositories/Interfaces/IQuizRepository.cs
@@ -0,0 +1,18 @@
+﻿using CyberQuizGrupp1.SHARED.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.DAL.Repositories.Interfaces
+{
+    public interface IQuizRepository
+    {
+        //hämtar all quiz-data för en specifik subkategori.
+        //används när ett quiz startas för att ladda frågor och svar kopplade till subkategorin.
+        Task<SubCategoryModel?> GetQuizDataBySubCategoryIdAsync(int subCategoryId);
+
+        //sparar ett genomfört quizförsök i databasen.
+        //innehåller information som användare, resultat och tidsstämpel.
+        Task AddQuizAttemptAsync(QuizAttemptModel quizAttempt);
+    }
+}
