b724660ce5024cccedb76b747739f30c434fdc4d
LudvigSHub <Ludvig.salovuo@gmail.com>
2026-03-11 13:43:30 +0100

    Nya DTOs för AI delen

---
+++ CyberQuizGrupp1.SHARED/DTOs/CoachingItemDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.SHARED.DTOs
+{
+    public class CoachingItemDTO
+    {
+        public int SubCategoryId { get; set; }
+        public string SubCategoryName { get; set; } = string.Empty;
+    }
+}
