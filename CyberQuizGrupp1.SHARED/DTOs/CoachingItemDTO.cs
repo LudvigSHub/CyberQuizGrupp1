c9d84934521baa01dde22d7839ea4a461f1a4762
Filippa24 <filippa98b@hotmail.se>
2026-03-11 17:27:17 +0100

    CoachingItemDTO + CoachingResponseDTO

---
+++ CyberQuizGrupp1.SHARED/DTOs/CoachingItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.SHARED.DTOs
+{
+    public class CoachingItemDTO
+    {
+        //DTO för ett objekt (en subkategori som användaren ska få feedback på från AI) i Coaching-listan
+        public int SubCategoryId { get; set;  }
+        public string SubCategoryName { get; set; } = "";
+    }
+}
---
+++ CyberQuizGrupp1.SHARED/DTOs/CoachingResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuizGrupp1.SHARED.DTOs
+{
+    //DTO som UI använder för att visa info för användaren
+    public class CoachingResponseDTO
+    {
+        public int SubCategoryId { get; set; }
+        public string SubCategoryName { get; set; } = "";
+        public string StrengthSummary { get; set; } = "";
+        public string WeaknessSummary { get; set; } = "";
+        public string CoachText { get; set; } = "";
+
+    }
+}
