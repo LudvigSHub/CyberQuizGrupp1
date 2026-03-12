1ca71ecac9a541a94eeb2a98a2aabd99f9cf5b22
Abbaskhudayer <abbas.khudayer@hotmail.com>
2026-03-12 11:57:44 +0100

    Skapade ProgressController och lade till GetAllAsync i SubCategoryRepository API: -Skapade ProgressController med GET /api/progress/{userId} endpoint -Registrerade ProgressController i Program.cs DAL: -Lade till GetAllAsync-metod i ISubCategoryRepository -Implementerade GetAllAsync i SubCategoryRepository för att hämta alla subkategorier

---
+++ CyberQuizGrupp1.API/Controllers/ProgressController.cs
@@ -0,0 +1,31 @@
+﻿using CyberQuizGrupp1.BLL.Interfaces;
+using CyberQuizGrupp1.SHARED.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CyberQuizGrupp1.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProgressController : ControllerBase
+    {
+        private readonly IProgressService _progressService;
+
+        public ProgressController(IProgressService progressService)
+        {
+            _progressService = progressService;
+        }
+
+        // GET: api/progress/{userId}
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<UserProgressDTO>> GetProgressAsync(string userId)
+        {
+            // enkel validering
+            if (string.IsNullOrWhiteSpace(userId))
+                return BadRequest("userId cannot be empty");
+
+            var progress = await _progressService.GetUserProgressAsync(userId);
+
+            return Ok(progress);
+        }
+    }
+}
